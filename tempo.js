bd167e4c0f279d9347a7a62c21174c748743eff3
Marina Cottrell <marinacottrell@gmail.com>
2018-04-25 17:19:27 -0700

    Initial commit

---
+++ tempo.js
@@ -0,0 +1,12 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+var tempo;
+output.innerHTML = slider.value;// Display the default slider value
+var vid = document.getElementById("audio");
+
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    tempo = (this.value/100) + 1;
+    vid.playbackRate = tempo;
+}
