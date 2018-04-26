9e0838c5b024302733ab5c5cdae6aed458f23623
Marina Cottrell <marinacottrell@gmail.com>
2018-04-25 17:31:23 -0700

    updated name

--- tempo.js
+++ tempo.js
@@ -2,11 +2,11 @@ var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 var tempo;
 output.innerHTML = slider.value;// Display the default slider value
-var vid = document.getElementById("audio");
+var audio = document.getElementById("audio");
 
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
     tempo = (this.value/100) + 1;
-    vid.playbackRate = tempo;
+    audio.playbackRate = tempo;
 }
